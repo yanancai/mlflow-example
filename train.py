33786529771d91b773a1ec20b28bde24707ac122
yanancai <yanacai@microsoft.com>
2018-08-12 19:01:58 +0800

    1st version for demo 8-13

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+# Set up MLflow track server
+import mlflow
+mlflow_server = '52.179.167.153'
+mlflow_tracking_URI = 'http://' + mlflow_server + ':5000'
+print ("MLflow Tracking URI: %s" % (mlflow_tracking_URI))
+#mlflow.set_tracking_uri(mlflow_tracking_URI)
+mlflow.set_tracking_uri("file://D:/Source/mlflow-example")
 
 import os
 import warnings
@@ -12,55 +16,125 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 
-import mlflow
 import mlflow.sklearn
 
+# Load Diabetes datasets
+diabetes = datasets.load_diabetes()
+X = diabetes.data
+y = diabetes.target
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+# Create pandas DataFrame for sklearn ElasticNet linear_model
+Y = np.array([y]).transpose()
+d = np.concatenate((X, Y), axis=1)
+cols = ['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6', 'progression']
+data = pd.DataFrame(d, columns=cols)
 
+def plot_enet_descent_path(X, y, l1_ratio):
+    # Compute paths
+    eps = 5e-3  # the smaller it is the longer is the path
 
+    # Reference the global image variable
+    global image
+    
+    print("Computing regularization path using the elastic net.")
+    alphas_enet, coefs_enet, _ = enet_path(X, y, eps=eps, l1_ratio=l1_ratio, fit_intercept=False)
 
-if __name__ == "__main__":
+    # Display results
+    fig = plt.figure(1)
+    ax = plt.gca()
+
+    colors = cycle(['b', 'r', 'g', 'c', 'k'])
+    neg_log_alphas_enet = -np.log10(alphas_enet)
+    for coef_e, c in zip(coefs_enet, colors):
+        l1 = plt.plot(neg_log_alphas_enet, coef_e, linestyle='--', c=c)
+
+    plt.xlabel('-Log(alpha)')
+    plt.ylabel('coefficients')
+    title = 'ElasticNet Path by alpha for l1_ratio = ' + str(l1_ratio)
+    plt.title(title)
+    plt.axis('tight')
+
+    # Display images
+    image = fig
+    
+    # Save figure
+    fig.savefig("ElasticNet-paths.png")
+
+    # Close plot
+    plt.close(fig)
+
+    # Return images
+    return image    
+
+# train_diabetes
+#   Uses the sklearn Diabetes dataset to predict diabetes progression using ElasticNet
+#   The predicted "progression" column is a quantitative measure of disease progression one year after baseline
+#   http://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_diabetes.html
+def train_diabetes(data, in_alpha, in_l1_ratio):
+    # Evaluate metrics
+    def eval_metrics(actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
     warnings.filterwarnings("ignore")
     np.random.seed(40)
-
-    # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
-    data = pd.read_csv(wine_path)
 
     # Split the data into training and test sets. (0.75, 0.25) split.
     train, test = train_test_split(data)
 
-    # The predicted column is "quality" which is a scalar from [3, 9]
-    train_x = train.drop(["quality"], axis=1)
-    test_x = test.drop(["quality"], axis=1)
-    train_y = train[["quality"]]
-    test_y = test[["quality"]]
+    # The predicted column is "progression" which is a quantitative measure of disease progression one year after baseline
+    train_x = train.drop(["progression"], axis=1)
+    test_x = test.drop(["progression"], axis=1)
+    train_y = train[["progression"]]
+    test_y = test[["progression"]]
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
+    if float(in_alpha) is None:
+        alpha = 0.05
+    else:
+        alpha = float(in_alpha)
+    
+    if float(in_l1_ratio) is None:
+        l1_ratio = 0.05
+    else:
+        l1_ratio = float(in_l1_ratio)
+  
+    # Start an MLflow run; the "with" keyword ensures we'll close the run even if this cell crashes
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
-        predicted_qualities = lr.predict(test_x)
+    predicted_qualities = lr.predict(test_x)
 
-        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
+    # Print out ElasticNet model metrics
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
+    # Set tracking_URI first and then reset it back to not specifying port
+    # Note, we had specified this in an earlier cell
+    #mlflow.set_tracking_uri(mlflow_tracking_URI)
 
-        mlflow.sklearn.log_model(lr, "model")
+    # Log mlflow attributes for mlflow UI
+    mlflow.log_param("alpha", alpha)
+    mlflow.log_param("l1_ratio", l1_ratio)
+    mlflow.log_metric("rmse", rmse)
+    mlflow.log_metric("r2", r2)
+    mlflow.log_metric("mae", mae)
+    mlflow.sklearn.log_model(lr, "model")
+    
+    # Call plot_enet_descent_path
+    image = plot_enet_descent_path(X, y, l1_ratio)
+    
+    # Log artifacts (output files)
+    mlflow.log_artifact("ElasticNet-paths.png")
+
+def main():
+    loop = int(sys.argv[1]) if len(sys.argv) > 1 else 10  
+    for alpha in range(loop):
+        for l1_ratio in range(loop):
+            train_diabetes(data, alpha/loop, l1_ratio/loop)
