6c7c72042d81e32c70590ea7cb0abb2ca3a118b8
yanancai <yanacai@microsoft.com>
2018-08-12 21:51:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ import sys
 import pandas as pd
 import numpy as np
 from itertools import cycle
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
